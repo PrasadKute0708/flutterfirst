ce82c5f2890424da36a6a6302c569c690c777261
Prasad Kute <prasadkute0708@gmail.com>
2023-03-01 00:27:05 +0530

    i added the answerone and answertwo functions to onpressed command

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerone() {
+    print("you chose windows!");
+  }
+
+  void answertwo() {
+    print("you chose Apple!");
+  }
+
   Widget build(BuildContext context) {
     var questions = ['What\'s your favorite color?', 'what\'s your fav food?'];
     return MaterialApp(
@@ -13,9 +21,9 @@ class MyApp extends StatelessWidget {
           title: Text("Question and answers"),
         ),
         body: Column(children: [
-          Text("the questions"),
-          ElevatedButton(onPressed: null, child: Text('Ans 1')),
-          ElevatedButton(child: Text('Ans 2'), onPressed: null),
+          Text("Which is your favourite operating system?"),
+          ElevatedButton(child: Text('Windows'), onPressed: answerone),
+          ElevatedButton(child: Text('Apple'), onPressed: answertwo),
         ]),
       ),
     );
